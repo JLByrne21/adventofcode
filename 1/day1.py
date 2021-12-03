ead0c852ecc1126715f4a5f6b526f57e18047f87
BuildTools <jlbyrne@hotmail.co.uk>
2021-12-03 00:00:02 +0000

    Starting Advent of Code

---
+++ 1/day1.py
@@ -0,0 +1,12 @@
+def count_larger_than_prev(lst):
+    counter = 0
+    with open(lst, 'r') as numbers:
+        contents = numbers.read()
+        num_list = contents.splitlines()
+        for i in range(1, len(num_list)):
+            if num_list[i] > num_list[i-1]:
+                counter += 1
+        print(counter)
+
+filename = "numbers.txt"
+count_larger_than_prev(filename)
